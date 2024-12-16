284e7e611007604bcf3bb6b4e4e0fa0cadaa2bf2
Fran Ruiz Lebrón <122262348+FranRL03@users.noreply.github.com>
2024-12-16 09:50:50 +0000

    exercise done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let list = "";
+
+  pronoun.map(a => {
+    adj.map(b => {
+      noun.map(c => {
+        list += `
+        <li> ${a}${b}${c}.com</li>
+        `;
+        // console.log(a + b + c + ".com");
+      });
+    });
+  });
+
+  document.querySelector("#exercise").innerHTML = list;
 };
