cc2dfb5f78597d8b22f5ba3c6811b1b5b4da8db1
Fran Ruiz Lebrón <122262348+FranRL03@users.noreply.github.com>
2024-12-16 10:11:59 +0000

    improved exercise

--- src/app.js
+++ src/app.js
@@ -10,17 +10,32 @@ window.onload = function() {
   // console.log("Hello Rigo from the console!");
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
+  let noun = ["focus", "racoon", "heroes"];
 
   let list = "";
 
   pronoun.map(a => {
     adj.map(b => {
       noun.map(c => {
-        list += `
-        <li> ${a}${b}${c}.com</li>
-        `;
-        console.log(a + b + c + ".com");
+        if (c.endsWith("us")) {
+          list += `
+          <li> ${a}${b}${c.slice(0, -2)}.us</li>
+          `;
+          console.log(a + b + c.slice(0, -2) + ".us");
+        } else if (c.endsWith("es")) {
+          list += `
+          <li> ${a}${b}${c.slice(0, -2)}.es</li>
+          `;
+          console.log(a + b + c.slice(0, -2) + ".es");
+        } else {
+          list += `
+          <li> ${a}${b}${c}.com</li>
+          `;
+          console.log(a + b + c + ".com");
+        }
+        // list += `
+        // <li> ${a}${b}${c}.com</li>
+        // `;
       });
     });
   });
